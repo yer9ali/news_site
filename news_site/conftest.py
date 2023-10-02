1acebd02fd3dac6878593019b4d11d2e0d9020ad
alibyer <yer9ali@mail.ru>
2023-10-02 17:11:23 +0600

    first commit

---
+++ news_site/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from news_site.users.models import User
+from news_site.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
